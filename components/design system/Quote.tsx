59e604e334873bf3651424e50fed19aa5f2d81a3
Laura Garcia Dieguez <laugardie89@gmail.com>
2023-08-03 17:09:35 +0200

    Add avatars

--- components/design system/Quote.tsx
+++ components/design system/Quote.tsx
@@ -5,7 +5,7 @@ type QuoteProps = { children: React.ReactNode };
 const Quote = ({ children }: QuoteProps) => (
   <div className="relative mb-12">
     <QuoteIcon className="absolute right-full mr-8" />
-    <p className="font-bold italic font-inter text-3xl leading-10">
+    <p className="font-bold italic font-source text-3xl leading-10">
       {children}
     </p>
   </div>
