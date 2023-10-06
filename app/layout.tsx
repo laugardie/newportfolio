d6a94800d403e71217e8d15f63b4ac0b0fd5958d
Laura Garcia Dieguez <laugardie89@gmail.com>
2023-10-06 11:08:17 +0200

    Add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "./Header";
+import Script from "next/script";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -31,6 +32,19 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         <div className="mx-auto box-border">{children}</div>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-J7JSKSS7Q5"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-J7JSKSS7Q5');
+          `}
+        </Script>
       </body>
     </html>
   );
